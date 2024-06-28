e228427d6ab629da140b4b8b2a6b9e038dd7f26a
Kim-Lan <me@kim-lan.com>
2024-06-28 14:33:00 -0700

    Add posts to localStorage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,11 +1,14 @@
-// Access submit button element
+// Access form elements
+const usernameEl = document.getElementById('username');
+const postTitleEl = document.getElementById('post-title');
+const postContentEl = document.getElementById('post-content');
 const submitEl = document.getElementById('submit');
 
 // Create array to store posts
 const posts = [];
 
 // Obtain posts from localStorage if it exists
-function init() {
+function initPosts() {
   const storedPosts = JSON.parse(localStorage.getItem('posts'));
 
   if (storedPosts !== null) {
@@ -17,6 +20,27 @@ function init() {
 function addPost(event) {
   // Prevent default action
   event.preventDefault();
+
+  // Obtain input values
+  const username = usernameEl.value.trim();
+  const postTitle = postTitleEl.value.trim();
+  const postContent = postContentEl.value;
+
+  // Create post object
+  const post = {
+    username,
+    postTitle,
+    postContent
+  };
+
+  // Add post object to posts array
+  posts.push(post);
+
+  // Save posts to localStorage
+  storePosts();
+
+  // Reset form
+  resetForm();
 }
 
 // Store posts to localStorage
@@ -24,8 +48,14 @@ function storePosts() {
   localStorage.setItem('posts', JSON.stringify(posts));
 }
 
+function resetForm() {
+  usernameEl.value = '';
+  postTitleEl.value = '';
+  postContentEl.value = '';
+}
+
 // Add listener to submit element
 submitEl.addEventListener('click', addPost);
 
 // Initialize posts
-init();
+initPosts();
