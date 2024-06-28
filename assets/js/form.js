00fa670ee3d11abf70d9cab75e0019f8cc02976e
Kim-Lan <me@kim-lan.com>
2024-06-28 11:48:55 -0700

    Set up localStorage

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,11 +1,31 @@
 // Access submit button element
 const submitEl = document.getElementById('submit');
 
+// Create array to store posts
+const posts = [];
+
+// Obtain posts from localStorage if it exists
+function init() {
+  const storedPosts = JSON.parse(localStorage.getItem('posts'));
+
+  if (storedPosts !== null) {
+    posts = storedPosts;
+  }
+}
+
 // Action to be performed on submit button click
 function addPost(event) {
   // Prevent default action
   event.preventDefault();
 }
 
+// Store posts to localStorage
+function storePosts() {
+  localStorage.setItem('posts', JSON.stringify(posts));
+}
+
 // Add listener to submit element
 submitEl.addEventListener('click', addPost);
+
+// Initialize posts
+init();
