a7c18ed6fa6ec9bafc0021172900ad9f8ab150fa
Kim-Lan <me@kim-lan.com>
2024-06-30 16:56:39 -0700

    Add styling to posts

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -13,9 +13,30 @@ function renderPosts() {
   }
 
   for (let i = 0; i < posts.length; i++) {
+    // Create post div
     const postEl = document.createElement('div');
-    postEl.textContent = JSON.stringify(posts[i]);
+    postEl.classList.add('post');
+
+    // Create post title div
+    const postTitleEl = document.createElement('div');
+    postTitleEl.classList.add('post-title');
+    postTitleEl.textContent = posts[i].postTitle;
+    
+    // Create post content div
+    const postContentEl = document.createElement('div');
+    postContentEl.classList.add('post-content');
+    postContentEl.textContent = posts[i].postContent;
+
+    // Create post author div
+    const postAuthorEl = document.createElement('div');
+    postAuthorEl.classList.add('post-author');
+    postAuthorEl.textContent = 'Posted by: ' + posts[i].username;
+
+    // Add elements to post container
     postContainerEl.appendChild(postEl);
+    postEl.appendChild(postTitleEl);
+    postEl.appendChild(postContentEl);
+    postEl.appendChild(postAuthorEl);
   }
 }
 
