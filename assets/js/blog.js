8140812ee8f401613b7a1cb9728b972c97af5264
Kim-Lan <me@kim-lan.com>
2024-06-30 16:39:55 -0700

    Add div to post-container for every post

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,13 +1,22 @@
+// Access post container element
+const postContainerEl = document.getElementById('post-container');
+
 // Create array to store posts
 let posts = [];
 
 // Obtain posts from localStorage if it exists
-function initPosts() {
+function renderPosts() {
   const storedPosts = JSON.parse(localStorage.getItem('posts'));
 
   if (storedPosts !== null) {
     posts = storedPosts;
   }
+
+  for (let i = 0; i < posts.length; i++) {
+    const postEl = document.createElement('div');
+    postEl.textContent = JSON.stringify(posts[i]);
+    postContainerEl.appendChild(postEl);
+  }
 }
 
-initPosts();
+renderPosts();
